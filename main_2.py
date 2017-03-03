2c582a91572a5342affbe4264f5ab427aa2b0376
bdynamite <bdynamite@yandex.ru>
2017-03-03 16:45:02 +0300

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -2,10 +2,8 @@ import requests
 import time
 import json
 
-TOKEN_URL = 'https://oauth.vk.com/blank.html#access_token=' \
-            '5794f313f436101f5eefdf8d152c8f0c62a7adb8e4b8a3dcb16040585d1ece4755a1d68e0061d914006a1' \
-            '&expires_in=0&user_id=289384'
-ACCESS_TOKEN = '5794f313f436101f5eefdf8d152c8f0c62a7adb8e4b8a3dcb16040585d1ece4755a1d68e0061d914006a1'
+TOKEN_URL = 'your_url'
+ACCESS_TOKEN = 'your_token'
 VERSION = '5.52'
 
 
