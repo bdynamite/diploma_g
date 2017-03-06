4acc6dd4036e0528539929ce1e8f735c2493c503
Anton Kupranis <bdynamite@yandex.ru>
2017-03-06 11:40:39 +0300

    Init

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
-import time
 import json
+from tqdm import tqdm
 
 TOKEN_URL = 'https://oauth.vk.com/blank.html#access_token=' \
             '5794f313f436101f5eefdf8d152c8f0c62a7adb8e4b8a3dcb16040585d1ece4755a1d68e0061d914006a1' \
@@ -9,31 +9,69 @@ ACCESS_TOKEN = '5794f313f436101f5eefdf8d152c8f0c62a7adb8e4b8a3dcb16040585d1ece47
 VERSION = '5.52'
 
 
-def get_friends(dict_params):
-    response = requests.get('https://api.vk.com/method/friends.get', dict_params)
-    return response.json()['response']['items']
+def get_users(dict_params, method):
+    dict_params['count'] = 1000
+    response = requests.get('https://api.vk.com/method/{method}'.format(method=method), dict_params).json()
+    if response['response']['count'] > 1000:
+        users = get_via_execute(dict_params, method, response['response']['count'])
+    else:
+        users = response['response']['items']
+    return users
 
 
-def get_followers(dict_params):
-    dict_params['count'] = 100
-    response = requests.get('https://api.vk.com/method/users.getFollowers', dict_params)
-    return response.json()['response']['items']
+def get_via_execute(dict_params, method, number):
+    number_of_query = [25000 for x in range(number // 25000)]
+    number_of_query.append(number % 25000)
+    users = []
+    offset = 0
+    for i in tqdm(number_of_query):
+        number_of_execute = round((i + 499) / 1000)
+        execute = 'var members = 0;' \
+                  'var i = {i};' \
+                  'var offset = {offset};' \
+                  'while (i > 0)' \
+                  '{open_bracket}' \
+                  'members = members + ' \
+                  'API.{method}({open_bracket}"user_id": {user_id}, "v": {v}, ' \
+                  '"count": "1000", "offset": offset{close_bracket}).items;' \
+                  'offset = offset + 1000;' \
+                  'i = i - 1;' \
+                  '{close_bracket}' \
+                  'return members;'.format(i=number_of_execute, open_bracket='{', method=method, offset=offset,
+                                           user_id=target_id, v=dict_params['v'], close_bracket='}')
+        offset += i
+        dict_params['code'] = execute
+        response = requests.get('https://api.vk.com/method/execute', dict_params).json()
+        users.extend(response['response'].split(','))
+    return users
 
 
 def get_groups(users, dict_params):
     all_groups = []
-    for i, user in enumerate(users):
-        if (i + 1) % 3 == 0: # для удовлетворения ограничениям API по количеству запросов в секунду
-            time.sleep(1)
-        params['user_id'] = user
-        groups = requests.get('https://api.vk.com/method/groups.get', dict_params).json()
-        if not groups.get('error'):
-            print(groups)
-            continue
-        all_groups.extend(groups['response']['items'])
-        print('{}/{}'.format(i + 1, len(users)))
-    print(len(all_groups))
-    print(len(set(all_groups)))
+    users = list(map(int, users))
+    number_of_query = [25 for x in range(len(users) // 25)]
+    number_of_query.append(len(users) % 25)
+    for i,number in tqdm(enumerate(number_of_query)):
+        users_slice = users[i * 25:(i + 1) * 25]
+        execute = 'var groups = [];' \
+                  'var i = {i};' \
+                  'var users = {users};' \
+                  'while (i > 0)' \
+                  '{open_bracket}' \
+                  'groups.push(API.{method}({open_bracket}"user_id": users[i-1], "v": {v}, ' \
+                  '"count": "1000", "extended": "1"{close_bracket}).items);' \
+                  'i = i - 1;' \
+                  '{close_bracket}' \
+                  'return groups;'.format(i=number, open_bracket='{', method='groups.get',
+                                          users=[*users_slice], v=dict_params['v'], close_bracket='}')
+        dict_params['code'] = execute
+        response = requests.get('https://api.vk.com/method/execute', dict_params).json()
+        for element in response['response']:
+            try:
+                elements_list = [(x['id'], x['screen_name']) for x in element]
+                all_groups.extend(elements_list)
+            except TypeError:
+                pass
     return all_groups
 
 
@@ -43,22 +81,22 @@ def save_json(data):
 
 
 def make_list_of_top_n_groups(groups, n):
-    list_of_dicts = [{'title':x, 'count':groups.count(x)} for x in set(groups)]
-    list_of_dicts = sorted(list_of_dicts, key=lambda x: x['count'], reverse=True)
+    list_of_dicts = [{'title': x[1], 'id': x[0], 'count': groups.count(x)} for x in tqdm(set(groups))]
+    list_of_dicts = sorted(list_of_dicts, key=lambda x: x['count'], reverse=False)
     return list_of_dicts[:n]
 
 
 target_id = input('Введите id пользователя: ')
 params = dict(access_token=ACCESS_TOKEN, v=VERSION, user_id=target_id)
 
-followers = get_followers(params)
-followers.extend(get_friends(params))
-list_of_groups = get_groups(followers, params)
+followers = get_users(params.copy(), 'users.getFollowers')
+followers.extend(get_users(params.copy(), 'friends.get'))
+
+list_of_groups = get_groups(followers, params.copy())
 list_of_groups = make_list_of_top_n_groups(list_of_groups, 10)
 save_json(list_of_groups)
 
-print(list_of_groups)
+print('Success!')
 
 
 
-'https://vk.com/326374'
